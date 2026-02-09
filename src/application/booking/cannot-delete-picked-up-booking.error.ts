24fdf937b66443ec29389db78313e88dd5a27665
Gdev <125574259+gisachris@users.noreply.github.com>
2026-02-09 11:23:05 +0200

    Add CannotDeletePickedUpBookingError for booking deletion validation

---
+++ src/application/booking/cannot-delete-picked-up-booking.error.ts
@@ -0,0 +1,9 @@
+import { type BookingID } from './booking'
+
+export class CannotDeletePickedUpBookingError extends Error {
+  public constructor(bookingId: BookingID) {
+    super(
+      `Cannot delete booking ${bookingId} because it is in PICKED_UP state`,
+    )
+  }
+}
