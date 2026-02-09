b24686e2053c9f7754bea01a9a0002f4fc5d0081
Gdev <125574259+gisachris@users.noreply.github.com>
2026-02-09 11:22:55 +0200

    Add create and delete methods to IUserService

--- src/application/user/user.service.interface.ts
+++ src/application/user/user.service.interface.ts
@@ -8,4 +8,12 @@ export abstract class IUserService {
   public abstract find(id: UserID): Promise<User | null>
 
   public abstract findByName(name: string): Promise<User | null>
+
+  public abstract create(data: {
+    name: string
+    passwordHash: string
+    role: string
+  }): Promise<User>
+
+  public abstract delete(id: UserID, currentUserId: UserID): Promise<void>
 }
